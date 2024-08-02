79b681bc9d4e777f3d77a507b694e5112e5c2e73
weikaiqiang <weikaiqiang@xmov.ai>
2024-08-02 10:16:05 +0800

    xiaoqiang-切换镜像源

--- test.py
+++ test.py
@@ -18,10 +18,9 @@ from libs.prompts.main import prompt
 
 messages = [
     {'role': 'system', 'content': prompt.strip()},
-    {'role': 'user', 'content': '设置录制状态关闭'}]
+    {'role': 'user', 'content': '帮我设置导播合成模式为两合成二等分'}]
 
 start_time = time.time()
-print(prompt)
 print(ChatClient()._chat(messages=messages))
 print(time.time() - start_time)
 
