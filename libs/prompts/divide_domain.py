0e847c3a0bc216e1cb59b225f38efd65e8d47569
weikaiqiang <weikaiqiang@xmov.ai>
2024-09-05 18:43:04 +0800

    xiaoqiang-bug解决

--- libs/prompts/divide_domain.py
+++ libs/prompts/divide_domain.py
@@ -25,19 +25,20 @@ class DivideDomainPrompt(BasePrompt):
         我需要你协助我根据聊天内容提取成标准的JSON数据
         提取规则如下：
         一共有三个垂域：
-            IOT垂域：实现对{% for iot in data.DEVICE_LIST %}{{iot.key}}、{% endfor %}的设备的操作 
+            IOT垂域：实现对{% for iot in data.DEVICE_LIST %}
+                {{iot.key}}的：{% for obj in iot.actions %} {{obj.command}}、{% endfor %}的操作{% endfor %}的设备的操作 
             会议控制垂域：预定会议、查看当前用户预约的会议、取消当前用户预定的某个会议
             闲聊垂域：大模型开放域问答
             JSON字段名称：domain
-            例如：
-            文本是：对xxx设备操作相关，对IOT设备操作相关domain字段的值是：iot-domain  
-            文本是：预订明天的会议室，对会议控制相关 domain字段的值是：meeting-domain  
-            不是控制IOT和会议相关的就属于闲聊垂域 domain字段的值是：chitchat-domain
+            实例：
+            文本是：对xxx设备操作相关，对IOT设备操作相关domain字段的值是: iot-domain 
+            文本是：预订明天的会议室，对会议控制相关domain字段的值是: meeting-domain
+            不是控制IOT和会议相关的就属于闲聊垂域domain字段的值是: chitchat-domain
             聊天内容: {% for obj in data.msgs %} 
             {{obj.role}}: {{obj.content}} {% endfor %}
             聊天记录肯可能为空 
             推理IOT设备推理时有一点需要注意下，如果用户表达的是 不打开xxx这样的否定词可以归类为关闭xxx设备
-            结合聊天记录推理用户最后一次输入的结果，直接输出可以使用的JSON原本数据不需要其它任何格式
+            结合聊天记录根据规则提取直接按照要求输出结果不需要其他任何输出
             """
 
     def _prompt(self):
--- test.py
+++ test.py
@@ -16,96 +16,16 @@ import time
 from libs.llm_chat import chat_client_ty_plus, chat_client_ty_turbo
 from libs.prompts import DivideDomainPrompt, MeetingControlPrompt, IotControlPrompt
 
-extra_paras = {'msgs': [
-    {
-        "role": "system",
-        "content": "好的，已左移摄像头"
-    },
-    {
-        "role": "user",
-        "content": "帮我下移摄像头"
-    },
-    {
-        "role": "system",
-        "content": "好的，已下移摄像头"
-    },
-    {
-        "role": "user",
-        "content": "帮我右移摄像头"
-    },
-    {
-        "role": "system",
-        "content": "好的，已右移摄像头"
-    },
-    {
-        "role": "user",
-        "content": "帮我非右移摄像头"
-    },
-    {
-        "role": "system",
-        "content": "好的，已左移摄像头"
-    },
-    {
-        "role": "user",
-        "content": "帮我复位摄像头"
-    },
-    {
-        "role": "user",
-        "content": "帮我打开空间人数传感器"
-    },
-    {
-        "role": "system",
-        "content": "好的，已打开空间人数传感器"
-    },
-    {
-        "role": "user",
-        "content": "帮我关闭空间人数传感器"
-    },
-    {
-        "role": "system",
-        "content": "好的，已关闭空间人数传感器"
-    },
-    {
-        "role": "user",
-        "content": "帮我重启空间人数传感器"
-    },
-    {
-        "role": "system",
-        "content": "好的，已重启空间人数传感器"
-    },
-    {
-        "role": "user",
-        "content": "空间人数传感器打开了吗"
-    },
-    {
-        "role": "user",
-        "content": "将第二个窗帘打开三分之一"
-    },
-    {
-        "role": "system",
-        "content": "好的，将第二个窗帘打开了三分之一"
-    },
-    {
-        "role": "user",
-        "content": "将第一个窗帘关闭三分之二"
-    },
-    {
-        "role": "system",
-        "content": "好的，将第一个窗帘关闭了三分之二"
-    },
-    {
-        "role": "user",
-        "content": "将第二个窗帘打开至一半"
-    }
-]}
+extra_paras = {'msgs': []}
 
 messages = [
-    {'role': 'user', 'content': IotControlPrompt(extra_paras=extra_paras).prompt},
-    {'role': 'user', 'content': "将窗帘打开到三分之二"}
+    {'role': 'user', 'content': DivideDomainPrompt(extra_paras=extra_paras).prompt},
+    {'role': 'user', 'content': "停止录制"}
 ]
 
-print(IotControlPrompt(extra_paras=extra_paras).prompt)
+print(DivideDomainPrompt(extra_paras=extra_paras).prompt)
+print(messages[1])
 for _ in range(10):
     start_time = time.time()
-    print(chat_client_ty_plus._chat(messages=messages))
+    print(chat_client_ty_turbo._json_chat(messages=messages))
     print(time.time() - start_time)
