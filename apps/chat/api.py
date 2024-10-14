1090ef3288fedf5c81e2e213af7e6fb2c39a901a
weikaiqiang <weikaiqiang@xmov.ai>
2024-10-14 15:01:12 +0800

    xiaoqiang-bug修复

--- apps/chat/api.py
+++ apps/chat/api.py
@@ -6,17 +6,19 @@ File: api.py
 
 Author: xiaoqiang
 
-Explanation: 
+Explanation:
 
 Date: 2024/8/3 12:30
 
 """
-
+import json
 import logging
 from fastapi import APIRouter
 from utils.sys_format_res import async_normal_response
 from .req_data import ChatReqData
-from libs.llm_chat.main import ChatClient
+from libs.llm_chat import chat_client_ty_plus
+from utils.sys_db_connect import app_redis
+from sys_config import CONFIG
 
 logger = logging.getLogger('logger')
 router = APIRouter()
@@ -26,6 +28,15 @@ router = APIRouter()
 @async_normal_response
 async def chat(req_data: ChatReqData):
     # 闲聊接口
-    messages = req_data.chat_messages
-    ans = await ChatClient().chat(messages=messages)
+
+    chat_str = await app_redis.get(name=f'{req_data.chat_id}_chat')
+    messages = []
+    if chat_str:
+        chat_obj = json.loads(chat_str)
+        messages.extend(chat_obj)
+    messages.extend([{'role': obj.role, 'content': obj.content} for obj in req_data.chat_messages])
+    ans = await chat_client_ty_plus.chat(messages=messages)
+    messages.append({'role': 'system', 'content': ans})
+    await app_redis.set(name=f'{req_data.chat_id}_chat', value=json.dumps(messages, ensure_ascii=False),
+                        ex=CONFIG.CONVERSATION['max_history_num'])
     return ans
